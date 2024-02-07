b0f8f4d4c390e7c84bfc4f76c70eb6e7864754f2
mohit <mohitb12363@gamil.com>
2024-02-07 14:03:40 +0530

    create signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/singup", signup);
+router.post("/singin", signin);
 
 export default router
